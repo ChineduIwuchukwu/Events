4e1a8819bd17d531f80e8aa92bdc51fd1008a9bb
Chinedu Iwuchukwu <iwuchukwuchinedu57@gmail.com>
2025-03-15 12:28:28 +0100

    git add .

--- script.js
+++ script.js
@@ -7,13 +7,37 @@ console.log("JavaScript Events");
 //   result.innerHTML = "Hello World";
 // });
 
-const addButton = document.getElementById("add-btn");
+// const addButton = document.getElementById("add-btn");
 
-addButton.addEventListener("click", (event) => {
+// addButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//   const num1 = document.getElementById("num1");
+//   const num2 = document.getElementById("num2");
+//   const result = document.getElementById("result");
+
+//   const addition = parseInt(num1.value) + parseInt(num2.value);
+//   result.innerHTML = addition;
+// });
+
+const contactForm = document.getElementById("contact");
+
+contactForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const num1 = document.getElementById("num1");
-  const num2 = document.getElementById("num2");
 
-  console.log("Num 1" num1.value);
-  console.log("Num 2" num2.value);
+  const fullName = document.getElementById("fullname");
+  const email = document.getElementById("email");
+  const age = document.getElementById("age");
+  const message = document.getElementById("message");
+
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `        
+          <td>${fullName.value}</td>
+          <td>${email.value}</td>
+          <td>${age.value}</td>
+          <td>${message.value}</td>
+      `;
+
+  const tableBody = document.getElementById("table-body");
+  tableBody.appendChild(tableRow);
 });
